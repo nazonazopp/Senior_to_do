c6dee1a8d97e9d13db9f0e341cd32fc0c769a3df
nazonazopp <61644930+nazonazopp@users.noreply.github.com>
2020-05-27 23:10:55 +0900

    Implementing senior-to-do graph introduction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
   def after_sign_in_path_for(resource)
     posts_path
   end
 
+  
+
 private
     def sign_in_required
         redirect_to new_user_session_url unless user_signed_in?
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email])
     end
 
 
